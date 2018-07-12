40e272ece22df2e8ae0d7cdc9392575e08b1e58a
Stefanie Torossian <stefanietorossian@gmail.com>
2018-07-12 19:28:48 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,11 +1,13 @@
 var index = require("./index.js")
 
+//Name
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received:Joe")
 }
 
+// Height
 if (index.height === 70) {
     console.log("The height is correct")
 } else {
